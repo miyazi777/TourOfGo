9e43664bea45866402fc9dc5fede8f0704e9c255
miyazi777 <m71203@gmail.com>
2017-04-09 13:33:54 +0900

    add source

---
+++ more_type/range_continued.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+	for i := range pow { // index only
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value) // 1 2 4 8 16 32 64 128 256 512
+	}
+}
--- more_type/slice-len-cap.go
+++ more_type/slice-len-cap.go
@@ -6,6 +6,15 @@ func main() {
 	s := []int{2, 3, 5, 7, 11, 13}
 	printSlice(s) // len=6 cap=6 [2,3,5,7,11,13]
 
+	s = s[:0]
+	printSlice(s) // len=6 cap=0 []
+
+	s = s[:4]
+	printSlice(s) // len=6 cap=4 [2, 3, 5, 7]
+
+	s = s[2:]
+	printSlice(s) // len=4 cap=4 [5, 7]
+
 }
 
 func printSlice(s []int) {
