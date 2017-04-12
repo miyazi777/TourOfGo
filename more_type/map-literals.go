3cb5f84a24ff9622ef06a0d896824a71f9db3809
miyazi777 <m71203@gmail.com>
2017-04-12 17:19:00 +0900

    add source

---
+++ more_type/map-literals.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{40.68, -74.39},
+	"Google":    Vertex{37.42, -122.08},
+}
+
+func main() {
+	fmt.Println(m)
+}
